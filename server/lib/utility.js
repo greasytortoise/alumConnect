16459539e1089d5e01ae5d178e9d11eee757d412
Matt Bresnan <bresnan.mw@gmail.com>
2016-04-07 15:59:22 -0700

    start token work

--- server/lib/utility.js
+++ server/lib/utility.js
@@ -1,4 +1,17 @@
 var bcrypt = require('bcrypt');
+var jwt = require('jwt-simple');
+
+
+exports.generateToken = function(userid, user) {
+  var expires = moment().add('days', 3).valueOf();
+  var token = jwt.encode({
+    iss: userid,
+    exp: expires
+  }, app.get('jwtTokenSecret'));
+
+  return {token: token, expires: expires, user: user.toJSON()}.toJSON();
+
+};
 
 exports.hashAndStore = function(password) {
   bcrypt.hash(password, 10, function(err, hash) {
