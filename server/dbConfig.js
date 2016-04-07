b8e91cd91071f2f91def6e05d5ffc1d1cb459d49
Daham Drake Wang <drakewang@gmail.com>
2016-04-06 22:01:22 -0700

    create test database with mysql

---
+++ server/dbConfig.js
@@ -0,0 +1,29 @@
+var knex = require('knex');
+var bookshelf = require('bookshelf');
+
+knex({
+  client: 'mysql',
+  connection: {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'alumConnectTest',
+    charset: 'utf8'
+  }
+});
+
+var db = bookshelf(knex);
+
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function(user) {
+      user.increments('id').primary();
+      user.string('username', 100);
+      user.string('password', 100);
+    }).then(function(table) {
+      console.log('Table is created!', table);
+    });
+  }
+});
+
+module.exports = db;
