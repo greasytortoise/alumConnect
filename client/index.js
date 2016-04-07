c96d6eb6771d3475afd358ad3c2fe772a4ec3b4b
Alamelu Palaniappan <alam.palaniappan@gmail.com>
2016-04-07 15:59:17 -0700

    work on route login page

--- client/index.js
+++ client/index.js
@@ -3,11 +3,13 @@ import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 import App from './components/App'
 import User from './components/User'
+import Login from './components/Login'
 
 
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App}/>
     <Route path="/user" component={User}/>
+    <Route path="/login" component={Login}/>
   </Router>
 ), document.getElementById('app'))
