0dfb6ca08593020a351ecad46c2733f3ce36dc86
Daham Drake Wang <drakewang@gmail.com>
2016-04-07 12:18:31 -0700

    implement model and collections

---
+++ server/collections/users.js
@@ -0,0 +1,7 @@
+var db = require('../config');
+var User = require('../models/user');
+
+var Users = new db.Collection();
+Users.model = User;
+
+module.exports = Users;
---
+++ server/models/network.js
@@ -0,0 +1,12 @@
+var db = require('../dbConfig');
+var User = require('./user');
+
+var Network = db.Model.extend({
+  tableName: 'Networks',
+  hasTimestamps: true,
+  user: function() {
+    return this.hasOne(User);
+  }
+});
+
+module.export = Network;
