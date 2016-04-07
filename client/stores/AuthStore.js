4e48194b6dd612ddad9523225a1fa78a116d1b40
Matt Bresnan <bresnan.mw@gmail.com>
2016-04-07 12:45:50 -0700

    add AuthStore

---
+++ client/stores/AuthStore.js
@@ -0,0 +1,87 @@
+import Reflux from 'reflux';
+import Actions from 'actions/AuthActions';
+
+var AuthStore = Reflux.createStore({
+  listenables: Actions,
+
+  init () {
+    this.jwtAlum = localStorage.getItem('jwtAlum');
+
+    this.claims = this.parseJwt();
+    this.error = false;
+    this.loading = false;
+  }
+
+  getState() {
+    return {
+      loading: this.loading,
+      error: this.error,
+      user: this.userFromClaims(),
+      loggedIn: this.loggedIn()
+
+    };
+  }
+
+  userFromClaims(){
+    return this.claims;
+  }
+
+  loggedIn(){
+    return this.claims !== null;
+  }
+
+  changed(){
+    this.trigger(this.getState());
+  }
+
+  onLogin(email, password){
+    this.loading = true;
+    this.changed();
+
+/*    MAKE API CALL HERE
+    MAKE API CALL HERE
+    MAKE API CALL HERE*/
+
+  }
+
+  onLoginCompleted(authResponse) {
+    if(authResponse) {
+      this.jwtAlum = AuthResponse.jwtAlum;
+      this.claims = this.parseJwt();
+      this.error = false;
+
+      localStorage.setItem('jwtAlum', this.jwtAlum);
+    } else {
+      this.error = "CANT STUMP THE TRUMP. (Invalid email/pw)";
+    }
+
+    this.loading = false;
+    this.changed();
+  }
+
+  ongLogout(){
+    this.jwt = null;
+    this.claims = null;
+    this.error = false;
+    this.loading = false;
+    localStorage.removeItem('jwtAlum');
+  }
+
+  parseJwt(){
+    if(this.jwtAlum !== null) {
+      return JSON.parse(atob(this.jwtAlum.split('.')[1]));
+    } else {
+      return null;
+    }
+  }
+
+});
+
+module.exports = AuthStore
+
+
+
+
+
+
+
