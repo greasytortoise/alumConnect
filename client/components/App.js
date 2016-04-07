9782acca05a4dabc602ca24a8fe1fce9360c4768
Mike Jonas <Jonas@Jonas.local>
2016-04-07 16:30:33 -0700

    Enable sitewide navigation

--- client/components/App.js
+++ client/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 // export default React.createClass({
 //   render() {
@@ -8,12 +9,19 @@ import React from 'react'
 
 class App extends React.Component {
   render() {
-    return <div>Hello, React Router!</div>
+    return(
+      <div>
+        <h2>Website title</h2>
+        <ul role="nav">
+          <li><Link to="/User">go to /user</Link></li>
+        </ul>
+
+        {/* add this */}
+        {this.props.children}
+
+      </div>
+    );
   }
 }
 
 module.exports = App;
-
-
-
-
