6b13d55dfd2a57189e81b56319d5568c1acfac0d
Mike Jonas <Jonas@Jonas.local>
2016-04-08 16:02:16 -0700

    Fix url reload issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  entry: './client/index.js',
 
+  entry: './client/index.js',
   output: {
     path: __dirname + "/client",
     filename: '/bundle.js'
@@ -8,7 +8,14 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+      { test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query:
+        {
+          presets:['es2015', 'react']
+        }
+      }
     ]
   }
 }
