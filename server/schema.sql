1662e10d82a976a82c597f7c9cc51e397818627c
Daham Drake Wang <drakewang@gmail.com>
2016-04-07 19:54:42 -0700

    update schema

--- server/schema.sql
+++ server/schema.sql
@@ -42,6 +42,27 @@ create table Bios (
   foreign key(user_id) references Users(id)
 );
 
+insert into Groups
+(group_name) 
+values 
+('HR40');
+
+insert into Users 
+(username, password, email, url_hash, public, permission, group_id) 
+values
+('Jonas', 'aaaaaa', 'j@mail.com', 'x2y2z2', 0, 0, 1),
+('Matt', 'bbbbbb', 'm@mail.com', 'i3j3k3', 0, 0, 1),
+('Alamu', 'cccccc', 'a@mail.com', 'n3m3o3', 0, 0, 1),
+('Drake', 'aaaaaa', 'd@mail.com', 'a1b1c1', 0, 0, 1);
+
+insert into Networks
+(network_name, username, user_id)
+values
+('facebook', 'dungeonMaster', 2),
+('twitter', 'notADungeonMaster', 2),
+('facebook', 'jonasBrothers', 1);
+
+
 /* Execute this file from the command line by typing:
  * mysql -u root < server/schema.sql
  * to create the database and the tables. */
