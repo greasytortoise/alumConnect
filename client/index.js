9aee5fbbe2181875b6f2755bb2386b28aea1a835
Mike Jonas <Jonas@Jonas.local>
2016-04-07 19:58:17 -0700

    clean urls and better routing

--- client/index.js
+++ client/index.js
@@ -1,94 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { browserHistory, Router, Route, Link } from 'react-router'
-import auth from './authHelpers.js'
+import { Router, Route, browserHistory } from 'react-router'
+import App from './components/App'
+import Users from './components/Users'
+import Edit from './components/Edit'
+import User from './components/User'
 import Login from './components/Login'
 import Profile from './components/Profile'
-
-const App = React.createClass({
-  getInitialState() {
-    return {
-      loggedIn: auth.loggedIn();
-    }
-  },
-
-  updateAuth(loggedIn) {
-    this.setState({
-      loggedIn: auth.loggedIn();
-    })
-  },
-
-  componentWillMount() {
-    auth.onChange = this.updateAuth(this.loggedIn);
-    auth.login()
-  },
-
-  render() {
-    return (
-      <div>
-        <ul>
-
-          <li><Link to="/dashboard">SPARKLY RAINBOW UNICORNS HERE!!!</Link></li>
-          <li>
-            {this.state.loggedIn ? (
-              <Link to="/logout">Log out</Link>
-            ) : (
-              <Link to="/login">Sign in</Link>
-            )}
-          </li>
-        </ul>
-        {this.props.children || <p>You are {!this.state.loggedIn && 'not'} logged in.</p>}
-      </div>
-    )
-  }
-})
-
-const Dashboard = React.createClass({
-  render() {
-    const token = auth.getToken()
-
-    return (
-      <div>
-        <h1>Dashboard</h1>
-        <p>COOL STUFF</p>
-        <p>{token}</p>
-      </div>
-    )
-  }
-})
-
-
-
-const Logout = React.createClass({
-  componentDidMount() {
-    auth.logout()
-  },
-
-  render() {
-  }
-})
 
 
 render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <Route path="/user" component={User}/>
+      <Route path="/users" component={Users}/>
+      <Route path="/users/:user" component={User}/>
       <Route path="/login" component={Login}/>
       <Route path="/profile" component={Profile}/>
       <Route path="/edit" component={Edit}/>
-      <Route path="login" component={Login} />
-      <Route path="logout" component={Logout} />
-      <Route path="dashboard" component={Dashboard} onEnter={requireAuth} />
     </Route>
   </Router>
 ), document.getElementById('app'))
-
-function requireAuth(nextState, replace) {
-  if (!auth.loggedIn()) {
-    replace({
-      pathname: '/login',
-      state: { nextPathname: nextState.location.pathname }
-    })
-  }
-}
-
