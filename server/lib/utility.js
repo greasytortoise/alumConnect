7138729f5a9375d29855c936d1b13e64c7d525fc
Matt Bresnan <bresnan.mw@gmail.com>
2016-04-08 14:36:28 -0700

    remove sessions

--- server/lib/utility.js
+++ server/lib/utility.js
@@ -33,21 +33,9 @@ exports.comparePass = function(password, callback) {
 
 
 exports.isLoggedIn = function(req) {
-  return req.session ? !!req.session.user : false;
+  //PROBABLY NEEDS ADJUSTMENT
+  return req.token ? !!req.token.user : false;
 };
 
-exports.checkUser = function(req, res, next){
-  if (!isLoggedIn(req)) {
-    res.redirect('/login');
-  } else {
-    next();
-  }
-};
 
-exports.createSession = function(req, res, newUser) {
-  return req.session.regenerate(function() {
-      req.session.user = newUser;
-      res.redirect('/');
-    });
-};
 
