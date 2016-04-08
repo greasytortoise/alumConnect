cc369f80c9d3f555e66a66667a341eefb45afced
Alamelu Palaniappan <alam.palaniappan@gmail.com>
2016-04-08 15:49:27 -0700

    commit before pull rebase

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Edit from './Edit.js';
+import Edit from './Edit';
 
 import { Link } from 'react-router'
 
@@ -8,7 +8,8 @@ class Profile extends React.Component {
   constructor (props) {
     super (props);
     this.state = {
-      currentProfile: null
+      currentProfile: null,
+      editing: 0
     };
   }
 
@@ -18,40 +19,43 @@ class Profile extends React.Component {
 
   _getUserProfile() {
     //request to server to get user profile
-    this.setState({
-      // this.currentProfile: userProfile;
-    })
+    if (this.editing === 0) {
+      return (
+        <div>
+          <Link to="/edit" onClick={this._handleEditProfileClick}>Edit Profile</Link>
+          <h3>Preferred name</h3>
+          <p>The Donald</p>
+          <img src={"../mockups/assets/donaldtrump.png"} className="photo" />
+
+          <h3>What have you been up to before HackReactor?</h3>
+          <p>Two years ago I moved from Virginia to LA to start working at Accenture as a security technology consultant. I had planned to  take a leave of absence for a few months to study python until one of my coworkers told me about HR. This was about 6  months ago. Ever since then I've been studying, and eventually took the leave so I could focus on getting admitted into the  program. I quickly noticed that "full time" studying actually just means a little bit of studying, and a lot of Netflix.</p>
+
+          <h3>What city are you coming from to be here at HR?</h3>
+          <p>I relocated back home to VA temporarily, but I've been in LA for the past two years.</p>
+
+          <h3>Tech interests?</h3>
+          <p>I've always wanted to get into programming, but never fully committed to learning. This past year I finally decided it was time to pick up a new skill set, because I was getting really bored of technology consulting. Some of my tech interest are cyber security, data mining, and machine learning. After my programming skills are better I also want to learn as much as I can about making hardware.</p>
+
+          <h3>Coding experience?</h3>
+          <p>I majored in IT in college, and took a few entry level classes for java and php. Since then I've taken java, C++, JS, and python classes online, which were all way more informative than what I learned in college. I picked up basic level linux commands at work since we use oracle's security suite for installs/configs/administration.</p>
+
+          <h3>Fun random stuff?</h3>
+          <p>I love to travel and experience new cultures and food. Like a few other people in the cohort I also scuba dive! After I saw Men of Honor I was always inspired to get my scuba cert one day. When I'm not studying I'm either at the gym, eating, cooking, or doing something else fitness related. I'm obsessed with fitness, and would like to eventually get into competing or fitness modeling.</p>
+
+        </div>
+      );
+    } else {
+      // return ();
+    }
   }
 
   _handleEditProfileClick(event) {
     event.preventDefault();
+    this.setState({editing: 1});
   }
 
   render() {
-    return (
-      <div>
-        <Edit handleEditProfileClick={this._handleEditProfileClick} />
-        <h3>Preferred name</h3>
-        <p>The Donald</p>
-        <img src={"../mockups/assets/donaldtrump.png"} className="photo" />
-
-        <h3>What have you been up to before HackReactor?</h3>
-        <p>Two years ago I moved from Virginia to LA to start working at Accenture as a security technology consultant. I had planned to  take a leave of absence for a few months to study python until one of my coworkers told me about HR. This was about 6  months ago. Ever since then I've been studying, and eventually took the leave so I could focus on getting admitted into the  program. I quickly noticed that "full time" studying actually just means a little bit of studying, and a lot of Netflix.</p>
-
-        <h3>What city are you coming from to be here at HR?</h3>
-        <p>I relocated back home to VA temporarily, but I've been in LA for the past two years.</p>
-
-        <h3>Tech interests?</h3>
-        <p>I've always wanted to get into programming, but never fully committed to learning. This past year I finally decided it was time to pick up a new skill set, because I was getting really bored of technology consulting. Some of my tech interest are cyber security, data mining, and machine learning. After my programming skills are better I also want to learn as much as I can about making hardware.</p>
-
-        <h3>Coding experience?</h3>
-        <p>I majored in IT in college, and took a few entry level classes for java and php. Since then I've taken java, C++, JS, and python classes online, which were all way more informative than what I learned in college. I picked up basic level linux commands at work since we use oracle's security suite for installs/configs/administration.</p>
-
-        <h3>Fun random stuff?</h3>
-        <p>I love to travel and experience new cultures and food. Like a few other people in the cohort I also scuba dive! After I saw Men of Honor I was always inspired to get my scuba cert one day. When I'm not studying I'm either at the gym, eating, cooking, or doing something else fitness related. I'm obsessed with fitness, and would like to eventually get into competing or fitness modeling.</p>
-
-      </div>
-    );
+    this._getUserProfile();
   }
 }
 
