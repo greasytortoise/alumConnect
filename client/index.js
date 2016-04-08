8071a2726ea9a5e5edc0f6ee0b65c90a872c6a06
Matt Bresnan <bresnan.mw@gmail.com>
2016-04-08 12:33:22 -0700

    sadasd

--- client/index.js
+++ client/index.js
@@ -21,7 +21,7 @@ render((
       <Route path="/edit" component={Edit}/>
       <Route path="/login" component={Login} />}
       <Route path="/logout" component={Login}  onEnter={auth.logout}/>
-   </Route>
+    </Route>
   </Router>
 ), document.getElementById('app'))
 
