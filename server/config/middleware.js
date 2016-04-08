04c5ca57f124753102c74a344d3808460eefd16d
Matt Bresnan <bresnan.mw@gmail.com>
2016-04-08 16:22:18 -0700

    stuff/things

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -4,4 +4,11 @@ module.exports = function(app, express) {
   app.use(bodyParser.json());
   app.use(express.static(__dirname + '/../../client'));
   app.set('jwtTokenSecret', 'DONALD_TRUMP_HUGE_HANDS_NO_PROBLEM');
+  
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  }); 
+
 };
