87d34370e8da8f0ec110934587a2defa0b0fc3b3
Matt Bresnan <bresnan.mw@gmail.com>
2016-04-07 20:33:15 -0700

    added hashing

--- client/authHelpers.js
+++ client/authHelpers.js
@@ -1,28 +1,41 @@
+import CryptoJS from '../node_modules/crypto-js/crypto-js.js'
+
 module.exports = {
-  login(email, pass, cb) {
-    cb = arguments[arguments.length - 1]
-    if (localStorage.token) {
-      if (cb) cb(true)
-      this.onChange(true)
-      return
-    }
+  login(email, password) {
+    var hash = CryptoJS.SHA256(password);
+
+/*    MAKE API CALL HERE
+    MAKE API CALL HERE
+    MAKE API CALL HERE*/
+    //ON SUCCESS TOKENIZE
+    //localStorage.setItem('jwtAlum', jwtAlum);
+
   },
 
   getToken() {
-    return localStorage.token
+    return localStorage.getItem('jwtAlum');
   },
 
-  logout(cb) {
-    delete localStorage.token
-    if (cb) cb()
-    this.onChange(false)
+  logout() {
+    localStorage.removeItem('jwtAlum');
+
+    //redirect home
   },
 
   loggedIn() {
-    return !!localStorage.token
+    return !!localStorage.getItem('jwtAlum');
   },
 
   onChange() {},
+
+  parseJwt(){
+    if(localStorage.getItem('jwtAlum') !== null) {
+      return JSON.parse(atob(localStorage.getItem('jwtAlum').split('.')[1]));
+    } else {
+      return null;
+    }
+  }
+
 
 }
  
