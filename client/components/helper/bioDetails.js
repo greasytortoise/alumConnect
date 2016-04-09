2a16c0ca70db98b22164a32bba327a1ac4c17867
Alamelu Palaniappan <alam.palaniappan@gmail.com>
2016-04-08 19:25:13 -0700

    adding bios component

---
+++ client/components/helper/bioDetails.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Bio extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      this.bio = this.props.user.bio
+    }
+  }
+
+  render() {
+    return (
+      
+    );
+  }
+
+
+}
