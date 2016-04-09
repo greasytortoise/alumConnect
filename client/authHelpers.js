107f46fbe7436625eb1bcc8d31c545ed7e5320f4
Matt Bresnan <bresnan.mw@gmail.com>
2016-04-08 19:45:06 -0700

    https fixes

--- client/authHelpers.js
+++ client/authHelpers.js
@@ -3,24 +3,7 @@ var request = require('superagent');
 module.exports = {
   login(email, password) {
 
-/*    MAKE API CALL HERE
-    MAKE API CALL HERE
-    MAKE API CALL HERE*/
-    // $.ajax({
-    //   url: '127.0.0.1:3000/login', 
-    //   type: 'POST',
-    //   data: JSON.stringify({email:email, password:password}),
-    //   success: function(data) {
-    //     console.log(data);
-    // //localStorage.setItem('jwtAlum', jwtAlum);       
-
-    //   },
-    //   error: function (data) {
-    //     console.error('Error fetching data');
-    //   }
-    // });
-    request
-      .post('127.0.0.1:3000/login')
+    request('POST', '/login')
       .send(JSON.stringify({email:email, password:password}))
       .end(function(err, res){
         if(err) {
