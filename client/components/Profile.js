62ad819e2a52b03639ccadb15c4fb96fdffdfef4
Alamelu Palaniappan <alam.palaniappan@gmail.com>
2016-04-10 17:05:01 -0700

    add bio component to render bio on user profile

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import Bio from './helper/bioDetails.js'
-import Image from './helper/Image.js'
+import Bio from './helperProfile/bioDetails.js'
+import Image from './helperProfile/Image.js'
 
 import { Link } from 'react-router'
+
+var request = require('superagent');
 
 class Profile extends React.Component {
 
@@ -11,7 +13,7 @@ class Profile extends React.Component {
 
     this.state = {
       image: 'Hello',
-      bioDetails: null,
+      bioDetails: {},
       editing: 0
     };
   }
@@ -21,20 +23,15 @@ class Profile extends React.Component {
   }
 
   _getUserProfile() {
-    //ajax request to get image and bioDetaills of the user /db/users/id
-    this.setState({
-      image: "../mockups/assets/donaldtrump.png",
-      bioDetails: {
-        id: 1,
-        name: "matt Bresnan",
-        before_hr: "Two years ago I moved from Virginia to LA to start working at Accenture as a security technology consultant. I had planned to take a leave of absence for a few months to study python until one of my coworkers told me about HR. This was about 6 months ago. Ever since then I've been studying, and eventually took the leave so I could focus on getting admitted into the program. I quickly noticed that 'full time' studying actually just means a little bit of studying, and a lot of Netflix.",
-        location: "I relocated back home to VA temporarily, but I've been in LA for the past two years.",
-        interest: "I've always wanted to get into programming, but never fully committed to learning. This past year I finally decided it was time to pick up a new skill set, because I was getting really bored of technology consulting. Some of my tech interest are cyber security, data mining, and machine learning. After my programming skills are better I also want to learn as much as I can about making hardware.",
-        experience: "I majored in IT in college, and took a few entry level classes for java and php. Since then I've taken java, C++, JS, and python classes online, which were all way more informative than what I learned in college. I picked up basic level linux commands at work since we use oracle's security suite for installs/configs/administration.",
-        fun_fact: "I love to travel and experience new cultures and food. Like a few other people in the cohort I also scuba dive! After I saw Men of Honor I was always inspired to get my scuba cert one day. When I'm not studying I'm either at the gym, eating, cooking, or doing something else fitness related. I'm obsessed with fitness, and would like to eventually get into competing or fitness modeling.",
-        user_id: 1
-      }
-    });
+    var url = '/db/bios/' + this.props.params.user;
+    request
+      .get(url)
+      .end((err, res) => {
+        this.setState({
+          image: "../mockups/assets/donaldtrump.png",
+          bioDetails: res.body.bio
+        })
+      });
   }
 
   _handleEditProfileClick(event, bioDetails) {
@@ -42,14 +39,14 @@ class Profile extends React.Component {
     this.setState({
       editing: 1,
       bioDetails: {
-        id: 1,
-        name: "matt Bresnan",
-        before_hr: "I moved from Virginia to LA to start working at Accenture as a security technology consultant. I had planned to take a leave of absence for a few months to study python until one of my coworkers told me about HR. This was about 6 months ago. Ever since then I've been studying, and eventually took the leave so I could focus on getting admitted into the program. I quickly noticed that 'full time' studying actually just means a little bit of studying, and a lot of Netflix.",
+        "id": 1,
+        "name": "matt Bresnan",
+        "before_hr": "I moved from Virginia to LA to start working at Accenture as a security technology consultant. I had planned to take a leave of absence for a few months to study python until one of my coworkers told me about HR. This was about 6 months ago. Ever since then I've been studying, and eventually took the leave so I could focus on getting admitted into the program. I quickly noticed that 'full time' studying actually just means a little bit of studying, and a lot of Netflix.",
         location: "I relocated back home to VA temporarily, but I've been in LA for the past two years.",
         interest: "I've always wanted to get into programming, but never fully committed to learning. This past year I finally decided it was time to pick up a new skill set, because I was getting really bored of technology consulting. Some of my tech interest are cyber security, data mining, and machine learning. After my programming skills are better I also want to learn as much as I can about making hardware.",
         experience: "took a few entry level classes for java and php. Since then I've taken java, C++, JS, and python classes online, which were all way more informative than what I learned in college. I picked up basic level linux commands at work since we use oracle's security suite for installs/configs/administration.",
-        fun_fact: "Love to travel and experience new cultures and food. Like a few other people in the cohort I also scuba dive! After I saw Men of Honor I was always inspired to get my scuba cert one day. When I'm not studying I'm either at the gym, eating, cooking, or doing something else fitness related. I'm obsessed with fitness, and would like to eventually get into competing or fitness modeling.",
-        user_id: 1
+        'fun_fact': "Love to travel and experience new cultures and food. Like a few other people in the cohort I also scuba dive! After I saw Men of Honor I was always inspired to get my scuba cert one day. When I'm not studying I'm either at the gym, eating, cooking, or doing something else fitness related. I'm obsessed with fitness, and would like to eventually get into competing or fitness modeling.",
+        'user_id': 1
       }
     });
   }
@@ -63,6 +60,8 @@ class Profile extends React.Component {
   }
 
   render() {
+    console.log(this.props.params.user);
+    console.log('resp', this.state.bioDetails);
     return (
       <div>
         <h3>Preferred name</h3>
@@ -70,8 +69,7 @@ class Profile extends React.Component {
         
         <Image image={this.state.image}/>
 
-        <Bio bioDetails={this.state.bioDetails} 
-          />
+        <Bio bioDetails={this.state.bioDetails} />
 
       </div>
     );
