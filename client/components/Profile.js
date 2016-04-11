3384fa25f3ef12fffcd83bb2273ba437e0a162ad
Alamelu Palaniappan <alam.palaniappan@gmail.com>
2016-04-10 17:11:07 -0700

    add bio component to render bio on user profile

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -34,30 +34,23 @@ class Profile extends React.Component {
       });
   }
 
-  _handleEditProfileClick(event, bioDetails) {
-    event.preventDefault();
-    this.setState({
-      editing: 1,
-      bioDetails: {
-        "id": 1,
-        "name": "matt Bresnan",
-        "before_hr": "I moved from Virginia to LA to start working at Accenture as a security technology consultant. I had planned to take a leave of absence for a few months to study python until one of my coworkers told me about HR. This was about 6 months ago. Ever since then I've been studying, and eventually took the leave so I could focus on getting admitted into the program. I quickly noticed that 'full time' studying actually just means a little bit of studying, and a lot of Netflix.",
-        location: "I relocated back home to VA temporarily, but I've been in LA for the past two years.",
-        interest: "I've always wanted to get into programming, but never fully committed to learning. This past year I finally decided it was time to pick up a new skill set, because I was getting really bored of technology consulting. Some of my tech interest are cyber security, data mining, and machine learning. After my programming skills are better I also want to learn as much as I can about making hardware.",
-        experience: "took a few entry level classes for java and php. Since then I've taken java, C++, JS, and python classes online, which were all way more informative than what I learned in college. I picked up basic level linux commands at work since we use oracle's security suite for installs/configs/administration.",
-        'fun_fact': "Love to travel and experience new cultures and food. Like a few other people in the cohort I also scuba dive! After I saw Men of Honor I was always inspired to get my scuba cert one day. When I'm not studying I'm either at the gym, eating, cooking, or doing something else fitness related. I'm obsessed with fitness, and would like to eventually get into competing or fitness modeling.",
-        'user_id': 1
-      }
-    });
-  }
+  // _handleEditProfileClick(event, bioDetails) {
+  //   event.preventDefault();
+  //   this.setState({
+  //     editing: 1,
+  //     bioDetails: {
+  //       
+  //     }
+  //   });
+  // }
 
-  _handleChangeImage(event, image) {
-    event.preventDefault();
-    this.setState({
-      editing: 1,
-      image: image
-    });
-  }
+  // _handleChangeImage(event, image) {
+  //   event.preventDefault();
+  //   this.setState({
+  //     editing: 1,
+  //     image: image
+  //   });
+  // }
 
   render() {
     console.log(this.props.params.user);
