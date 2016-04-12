64fdae7e29d78328f801594a0ae47ef2ea8cf93f
mikejonas <motion@gmail.com>
2016-04-12 01:13:52 -0700

    html viewport settings for mobile

--- client/components/App.js
+++ client/components/App.js
@@ -4,15 +4,12 @@ import NavLink from './NavLink'
 import NavigationBar from './helpers/NavigationBar.js'
 import { Grid } from 'react-bootstrap';
 
-
 class App extends React.Component {
   render() {
     return(
       <Grid>
         <NavigationBar />
         <div id="main">
-          <h2>Website title</h2>
-
           {this.props.children}
         </div>
       </Grid>
