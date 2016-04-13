4b7c2d3b7ef1a3ca13ab4ffcacb500713fa81c45
Daham Drake Wang <drakewang@gmail.com>
2016-04-13 16:13:34 -0700

    modify routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -14,29 +14,50 @@ var Users = require('../collections/users');
 
 
 module.exports = function(app, express) {
-
+  // db/groups
   app.get('/db/groups', handler.fetchGroups);
-  app.get('/db/groups/:id', handler.fetchGroupId);
-  app.post('/db/groups', handler.postGroup);
+  app.get('/db/groups/group/:id', handler.fetchGroupId);
+  app.post('/db/groups', handler.createGroup);
+  app.post('/db/groups/group/:id', handler.modifyGroup);
+  app.delete('/db/groups/group/:id', handler.deleteGroup);
 
-  app.get('/db/groups', function(req, res) {
-    Groups.fetch()
-      .then(function(groups) {
-        res.json(groups);
-    });
-  });
-
+  // db/users
   app.get('/db/users', handler.fetchUsers);
-  app.get('/db/users/:id', handler.fetchUserId);
-  app.post('/db/users', handler.postUser);
+  app.get('/db/users/group/:id', handler.fetchUsersByGroup);
+  app.get('/db/users/user/:id', handler.fetchUserId);
+  app.post('/db/users', handler.createUser);
+  app.post('/db/users/user/:id', handler.modifyUser);
+  app.delete('/db/users/user/:id', handler.deleteUser);
 
-  app.get('/db/networks', handler.fetchNetworks);
-  app.get('/db/networks/:id', handler.fetchNetworkId);
-  app.post('/db/networks', handler.postNetwork);
+  // db/sites (previously networks)
+  app.get('/db/sites', handler.fetchSites);
+  app.get('/db/sites/site/:id', handler.fetchSiteId);
+  app.post('/db/sites', handler.createSite);
+  app.post('/db/sites/site/:id', handler.modifySite);
+  app.delete('/db/sites/site/:id', handler.deleteSite);
 
-  app.get('/db/bios', handler.fetchBios);
-  app.get('/db/bios/:id', handler.fetchBioId);
-  app.post('/db/bios', handler.postBio);
+  // db/fields
+  app.get('/db/fields', handler.fetchFields);
+  app.get('/db/fields/field/:id', handler.fetchFieldId);
+  app.post('/db/fields', handler.createField);
+  app.post('/db/fields/field/:id', handler.modifyField);
+  app.delete('/db/fields/field/:id', handler.deleteField);
+
+  // app.get('/db/groups', handler.fetchGroups);
+  // app.get('/db/groups/:id', handler.fetchGroupId);
+  // app.post('/db/groups', handler.postGroup);
+
+  // app.get('/db/users', handler.fetchUsers);
+  // app.get('/db/users/:id', handler.fetchUserId);
+  // app.post('/db/users', handler.postUser);
+
+  // app.get('/db/networks', handler.fetchNetworks);
+  // app.get('/db/networks/:id', handler.fetchNetworkId);
+  // app.post('/db/networks', handler.postNetwork);
+
+  // app.get('/db/bios', handler.fetchBios);
+  // app.get('/db/bios/:id', handler.fetchBioId);
+  // app.post('/db/bios', handler.postBio);
 
   app.post('/checktoken', util.getPermissions);
   
