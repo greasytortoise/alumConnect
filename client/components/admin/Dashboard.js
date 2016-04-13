7fcfa92611e7fbae3521b284a9fac34ce4a58d36
mikejonas <motion@gmail.com>
2016-04-13 00:36:15 -0700

    Setup dashboard sidebar

--- client/components/admin/Dashboard.js
+++ client/components/admin/Dashboard.js
@@ -5,13 +5,51 @@ import { Grid } from 'react-bootstrap';
 class Dashboard extends React.Component {
   render() {
     return(
-      <Grid>
+      <div id="wrapper">
+
         <ul role="nav">
           {/*<li><NavLink to="/dashboard" onlyActiveOnIndex>Dashboard</NavLink></li>
           <li><NavLink to="/dashboard/users">users</NavLink></li>*/}
         </ul>
-        {this.props.children}
-      </Grid>
+        <div id="sidebar-wrapper">
+          <ul className="sidebar-nav">
+            <li className="sidebar-brand">
+              <a href="#">
+                Start Bootstrap
+              </a>
+            </li>
+            <li>
+              <a href="#">Dashboard</a>
+            </li>
+            <li>
+              <a href="#">Shortcuts</a>
+            </li>
+            <li>
+              <a href="#">Overview</a>
+            </li>
+            <li>
+              <a href="#">Events</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+
+        <div id="page-content-wrapper">
+          <div className="container-fluid">
+            {this.props.children}
+          </div>
+        </div>
+
+      </div>
+
     );
   }
 }
