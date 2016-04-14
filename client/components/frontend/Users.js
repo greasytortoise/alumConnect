09d72059643d6fd8078d55b6bff9901becbc255b
mikejonas <motion@gmail.com>
2016-04-13 17:57:47 -0700

    retrieve groups from database

--- client/components/frontend/Users.js
+++ client/components/frontend/Users.js
@@ -9,13 +9,17 @@ class Users extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      users: []
+      users: [],
+      groups: []
     }
   }
 
   componentDidMount() {
     RestHandler.Get('/db/users', (err, res) => {
       this.setState({users: res.body})
+    });
+    RestHandler.Get('/db/groups', (err, res) => {
+      this.setState({groups: res.body})
     });
   }
 
@@ -24,7 +28,7 @@ class Users extends React.Component {
     return this.state.users.map(function(user, index) {
       var {username, id} = user
       return(
-        <Col xs={6} sm={4} md={4}>
+        <Col xs={6} sm={4} md={3} lg={3}>
           <div className="user-card">
             <Image
               src='https://pixabay.com/static/uploads/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
@@ -37,17 +41,26 @@ class Users extends React.Component {
     });
   }
   cohortList() {
+  }
 
+  groupSelect(evt, key) {
+    debugger;
+    console.log(key);
+  }
+
+  renderGroups(groupSelect) {
+    return this.state.groups.map (function(group) {
+      var {id, group_name} = group;
+      return(
+        <MenuItem key={id} eventKey={id} onSelect={groupSelect}>{group_name}</MenuItem>
+      );
+    });
   }
 
   render() {
     const innerDropdown = (
       <DropdownButton bsStyle='default' title='Cohort40'>
-        <MenuItem eventKey="40">Cohort40</MenuItem>
-        <MenuItem eventKey="39">Cohort39</MenuItem>
-        <MenuItem eventKey="38" active>Cohort38</MenuItem>
-        <MenuItem divider />
-        <MenuItem eventKey="0">All Cohorts</MenuItem>
+        {this.renderGroups(this.groupSelect)}
       </DropdownButton>
     );
 
