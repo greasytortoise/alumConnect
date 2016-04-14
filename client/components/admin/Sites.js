7905226ea86d6596e84420cd7a89fe4b738effb7
Alamelu Palaniappan <alam.palaniappan@gmail.com>
2016-04-14 10:25:00 -0700

    fix bugs on admin add groups, sites and clear form on submit

--- client/components/admin/Sites.js
+++ client/components/admin/Sites.js
@@ -20,26 +20,38 @@ class Sites extends React.Component {
 
   renderSites() {
     return this.state.sites.map(function(site) {
-      var {id, site_name} = site;
+      var {id, site_name, base_url} = site;
       return (
-        <ListGroupItem key={id}>{site_name}</ListGroupItem>
+        <ListGroupItem key={id} header="site_name">
+          {base_url}
+        </ListGroupItem>
       );
     });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    var site = this.refs.site.refs.input.value;
-    request('POST', '/db/sites')
-      .send({site_name: site})
-      .end(function(err, res){
-        if (err) {
-          console.error(err);
-        } //rerender all groups on success
-        // this.setState({
-        //   groups: res.body
-        // });
-      });
+    var site = this.refs.site.getValue();
+    var url = this.refs.url.getValue();
+
+    var siteInfo = {
+      site_name: site,
+      base_url: url
+    };
+
+    RestHandler.Post('/db/sites', siteInfo, (err, res) => {
+      if (err) {return err;}
+      this.setState({sites: this.state.sites.concat(res.body)})
+    });
+
+    this.clearForm();
+  }
+
+  clearForm() {
+    const fields = ['site', 'url'];
+    fields.map(field => {
+      this.refs[field].refs['input'].value = '';
+    });
   }
 
   render() {
@@ -50,8 +62,11 @@ class Sites extends React.Component {
           {this.renderSites()}
         </ListGroup>
         <form onSubmit={this.handleSubmit.bind(this)}>
+
           <Input type="text" label="Add Site" 
             placeholder="Enter site name" ref="site" />
+          <Input type="text"  ref="url" 
+            placeholder="Enter site url  example: https://www.github.com/" />
 
           <ButtonInput type="submit" value="Submit"/>
         </form>
