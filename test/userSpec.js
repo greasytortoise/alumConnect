660d1ad25fdb591afb040eecfd2f663cc85e1fd5
Daham Drake Wang <drakewang@gmail.com>
2016-04-15 22:17:49 -0700

    strategizing on user spec

---
+++ test/userSpec.js
@@ -0,0 +1,52 @@
+var chai = require('chai');
+var expect = chai.expect;
+var request = require('supertest');
+var Promise = require('bluebird');
+
+var handler = require('../server/lib/handler');
+var userController = require('../server/controllers/userController');
+var app = require('../server/app.js');
+
+var User = require('../server/models/user');
+var Users = require('../server/collections/users');
+
+var Group = require('../server/models/group');
+var Groups = require('../server/collections/groups');
+
+var UserSite = require('../server/models/userSite');
+var UserSites = require('../server/collections/userSites');
+
+var Bio = require('../server/models/bio');
+var Bios = require('../server/collections/bios');
+
+
+
+describe('Site Endpoint: ', function() {
+  describe('fetchUsers', function() {
+
+  });
+
+  describe('fetchUsersByGroup', function() {
+
+  });
+
+  describe('fetchUserInfo', function() {
+
+  });
+
+  describe('createUser', function() {
+
+  });
+
+  describe('modifyUser', function() {
+
+  });
+
+  describe('deleteUser', function() {
+    
+  });
+
+
+});
+
+
