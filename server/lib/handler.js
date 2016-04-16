cd54a82182a781418b2520ab61151358173a7963
Daham Drake Wang <drakewang@gmail.com>
2016-04-15 19:50:16 -0700

    include getAll, createAll, saveIds, deleteAll

--- server/lib/handler.js
+++ server/lib/handler.js
@@ -1,7 +1,35 @@
 var bodyParser = require('body-parser');
 var util = require('./utility.js');
+var Promise = require('bluebird');
 
 module.exports = {
+  getAll: function(attrs, Model) {
+    return Promise.map(attrs, function(attr) {
+      return Model.forge(attr).fetch();
+    })
+    .filter(function(models) {
+      return models;
+    });
+},
+
+  deleteAll: function(models) {
+    return Promise.map(models, function(model) {
+      return model.destroy();
+    });
+  },
+
+  createAll: function(attrs, Model) {
+    return Promise.map(attrs, function(attr) {
+      return Model.forge(attr).save();
+    });
+  },
+
+  saveIds: function(models) {
+    return models.map(function(model) {
+      return model.id;
+    });
+  },
+
   checkLogin: function(req, res) {
 
     User.where({email: req.body.email}).fetch().then(function(user){
