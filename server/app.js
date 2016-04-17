d19c35cac01ab71541d8c44502ed75a861752a64
Matt Bresnan <bresnan.mw@gmail.com>
2016-04-17 16:15:18 -0700

    sdadas

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var Promise = require('bluebird');
 var app = express();
 //things
 
-var LEX = require('letsencrypt-express').testing();
+var LEX = require('letsencrypt-express');
 
 var middleware = require('./config/middleware');
 var routes = require('./config/routes');
@@ -13,22 +13,23 @@ routes(app, express);
 
 
 
-var lex = LEX.create({
-  configDir: /*require('os').homedir()*/ './config',
-  approveRegistration: function (hostname, cb) {
-    cb(null, {
-      domains: [hostname],
-      email: 'bresnan.mw@gmail.com',
-      agreeTos: true,
-    });
-  }
-});
+// var lex = LEX.create({
+//   configDir: /*require('os').homedir()*/ './config',
+//   approveRegistration: function (hostname, cb) {
+//     cb(null, {
+//       domains: [hostname],
+//       email: 'bresnan.mw@gmail.com',
+//       agreeTos: true,
+//     });
+//   }
+// });
 
-lex.onRequest = app;
+// lex.onRequest = app;
 
-lex.listen([3000], [1337, 5001], function () {
-  var protocol = ('requestCert' in this) ? 'https': 'http';
-  console.log("Listening at " + protocol + '://localhost:' + this.address().port);
-});
+// lex.listen([3000], [1337, 5001], function () {
+//   var protocol = ('requestCert' in this) ? 'https': 'http';
+//   console.log("Listening at " + protocol + '://localhost:' + this.address().port);
+// });
+app.listen(3000);
 
 module.exports = app;
