db96012e15b5e92a1289ff6ba922a560dafd9508
Matt Bresnan <bresnan.mw@gmail.com>
2016-04-17 16:16:18 -0700

    sdadas

--- server/app.js
+++ server/app.js
@@ -13,23 +13,22 @@ routes(app, express);
 
 
 
-// var lex = LEX.create({
-//   configDir: /*require('os').homedir()*/ './config',
-//   approveRegistration: function (hostname, cb) {
-//     cb(null, {
-//       domains: [hostname],
-//       email: 'bresnan.mw@gmail.com',
-//       agreeTos: true,
-//     });
-//   }
-// });
+var lex = LEX.create({
+  configDir: /*require('os').homedir()*/ './config',
+  approveRegistration: function (hostname, cb) {
+    cb(null, {
+      domains: [hostname],
+      email: 'bresnan.mw@gmail.com',
+      agreeTos: true,
+    });
+  }
+});
 
-// lex.onRequest = app;
+lex.onRequest = app;
 
-// lex.listen([3000], [1337, 5001], function () {
-//   var protocol = ('requestCert' in this) ? 'https': 'http';
-//   console.log("Listening at " + protocol + '://localhost:' + this.address().port);
-// });
-app.listen(3000);
+lex.listen([3000], [1337, 5001], function () {
+  var protocol = ('requestCert' in this) ? 'https': 'http';
+  console.log("Listening at " + protocol + '://localhost:' + this.address().port);
+});
 
 module.exports = app;
