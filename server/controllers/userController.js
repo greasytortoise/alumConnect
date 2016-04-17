2c4ad882107a43ec45c8bd840163e64f103fcc61
Daham Drake Wang <drakewang@gmail.com>
2016-04-16 17:35:59 -0700

    fix modify method

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -10,6 +10,8 @@ var UserSites = require('../collections/userSites');
 var Bio = require('../models/bio');
 var Bios = require('../collections/bios');
 
+var Promise = require('bluebird');
+
 module.exports = {
   // http://localhost:3000/db/users
   fetchUsers: function(req, res) {
@@ -111,7 +113,6 @@ module.exports = {
     });
   },
   // http://localhost:3000/db/users
-  // need admin permission to create a user
   createUser: function(req, res) {
     var data = req.body;
 
@@ -120,7 +121,6 @@ module.exports = {
       .fetch()
       .then(function(group) {
         return new User({
-          // permission and public are not dealt with yet...
           username: data.user.username,
           password: data.user.password,
           email: data.user.email,
@@ -159,43 +159,101 @@ module.exports = {
   },
   // http://localhost:3000/db/users/user/:id
   // no error handling yet
+
   modifyUser: function(req, res) {
     var id = req.params.id;
     var data = req.body;
-    Group.where({group_name: data.user.group}).fetch().then(function(group) {
-      User.where({id: id}).fetch().then(function(user) {
-        return user.save({
-          username: data.user.username || user.get('username'),
-          password: data.user.password || user.get('password'),
-          email: data.user.email || user.get('email'),
-          image: data.user.image || user.get('image'),
-          url_hash: data.user.url || user.get('url_hash'),
-          Group_id: group.id
-        }).then(function(user) {
-          // site is not changeable (only its value is)
-          data.sites.forEach(function(site) {
-            UserSite.where({User_id: id, Site_id: site.id}).fetch().then(function(userSite) {
-              userSite.save({
-                rest_url: site.value || userSite.get('rest_url')
-              });
-            });
-          });
-          return user;
-        }).then(function(user) {
-          data.userInfo.forEach(function(info) {
-            Bio.where({User_id: id, Bio_Field_id: info.id}).fetch().then(function(bio) {
-              bio.save({
-                bio: info.value || bio.get('bio')
-              });
-            });
-          });
-        }).then(function() {
-          res.send(201);
-        });
-      });
-    });
 
+    // grab user and join groups + userSites + bios
+    User
+      .where({id: id})
+      .fetch({withRelated: ['groups', 'userSites', 'bios']})
+      .then(function(user) {
+
+        // join sites
+        user
+          .related('userSites')
+          .fetch({withRealted: ['sites']})
+          .then(function(userSites) {
+
+            // join bioFields
+            user
+              .related('bios')
+              .fetch({withRelated: ['bioFields']})
+              .then(function(bios) {
+                var group = user.related('groups');
+
+                // modify user info
+                user
+                  .save({
+                    username: data.user.username || user.get('username'),
+                    password: data.user.password || user.get('password'),
+                    email: data.user.email || user.get('email'),
+                    image: data.user.image || user.get('image'),
+                    url_hash: data.user.url || user.get('url_hash'),
+                    Group_id: group.id
+                  })
+                  .then(function(user) {
+                    // for each site in the array
+                      // if it exists, modify it
+                      // else, create it
+                    return Promise.map(data.sites, function(site) {
+                      return userSites
+                        .query(function(qb) {
+                          qb.where('User_id', '=', id)
+                            .andWhere('Site_id', '=', site.id);
+                        })
+                        .fetch()
+                        .then(function(userSite) {
+                          if (userSite.at(0)) {
+                            userSite.at(0).save({
+                              rest_url: site.value || userSite.get('rest_url')
+                            });
+                          } else {
+                            userSites.create({
+                              rest_url: site.value,
+                              User_id: user.id,
+                              Site_id: site.id              
+                            });
+                          }
+                        });
+                    });
+                  })
+                  .then(function() {
+                    // for each bio in the array
+                    // if it exists, modify it
+                    // else, create it
+                    return Promise.map(data.userInfo, function(info) {
+                      return bios
+                        .query(function(qb) {
+                          qb.where('User_id', '=', id)
+                            .andWhere('Bio_Field_id', '=', info.id);
+                        })
+                        .fetch()
+                        .then(function(bio) {
+                          if (bio.at(0)) {
+                            bio.at(0).save({
+                              bio: info.value || bio.get('bio')
+                            });
+                          } else {
+                            bios.create({
+                              bio: info.value,
+                              User_id: user.id,
+                              Bio_Field_id: info.id                             
+                            });
+                          }
+                        });
+                    });
+
+                  }).then(function() {
+                    res.send(201);
+                  });
+              });
+          });
+
+      });
   },
+
   // http://localhost:3000/db/users/user/:id
   deleteUser: function(req, res) {
     var id = req.params.id;
