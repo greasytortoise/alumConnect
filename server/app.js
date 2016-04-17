4ba30f85351af01cb85b218d8ef76fa764310e6b
Matt Bresnan <bresnan.mw@gmail.com>
2016-04-17 16:29:59 -0700

    bugfixes

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var Promise = require('bluebird');
 var app = express();
 //things
 
-var LEX = require('letsencrypt-express');
+var LEX = require('letsencrypt-express').testing();
 
 var middleware = require('./config/middleware');
 var routes = require('./config/routes');
