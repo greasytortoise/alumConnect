7df50c908d982c313f8e9ea404d4959758d5e9bb
Matt Bresnan <bresnan.mw@gmail.com>
2016-04-18 11:58:45 -0700

    asdsd

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -2,7 +2,6 @@ var knex = require('knex')({
   client: 'mysql',
   connection: {
     host: '127.0.0.1',
-    port: '3306',
     user: 'root',
     password: 'abcd1234',
     database: 'alumConnectTest',
