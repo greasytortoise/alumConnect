c300e09655464736ee0fa6e5acbca1a81a3ca5fd
Matt Bresnan <bresnan.mw@gmail.com>
2016-04-18 10:37:48 -0700

    asdsd

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -3,9 +3,14 @@ var knex = require('knex')({
   connection: {
     host: '127.0.0.1',
     user: 'root',
-    password: '',
+    password: 'abcd1234',
     database: 'alumConnectTest',
-    charset: 'utf8'
+    charset: 'utf8',
+    insecureAuth: true
+  },
+   pool: {
+    min: 0,
+    max: 666
   }
 });
 var bookshelf = require('bookshelf');
