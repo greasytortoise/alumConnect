dad390f003c60d6a786b61343549ef8bc5d08d44
Matt Bresnan <bresnan.mw@gmail.com>
2016-04-18 16:34:36 -0700

    https tweaks

--- server/app.js
+++ server/app.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var Promise = require('bluebird');
-var fs = require('fs');
 var app = express();
-var https = require('https');
+
 
 var LEX = require('letsencrypt-express');
-
-var ports = process.env.NODE_ENV === 'production'
-  ? [80, 443]
-  : [3442, 3443];
 
 var middleware = require('./config/middleware');
 var routes = require('./config/routes');
@@ -16,15 +11,23 @@ var routes = require('./config/routes');
 middleware(app, express);
 routes(app, express);
 
-var server = https.createServer(
-  {
-    key: fs.readFileSync('./tls/key.pem'),
-    cert: fs.readFileSync('./tls/cert.pem')
-  },
-  app
-);
 
-server.listen(ports[1][7]);
-app.listen(ports[0]);
+var lex = LEX.create({
+  configDir: '../tls/',
+  approveRegistration: function (hostname, cb) {
+    cb(null, {
+      domains: ['donkit.com'],
+      email: 'bresnan.mw@gmail.com',
+      agreeTos: true,
+    });
+  }
+});
+
+lex.onRequest = app;
+
+lex.listen([3000], [1337, 5001], function () {
+  var protocol = ('requestCert' in this) ? 'https': 'http';
+  console.log("Listening at " + protocol + '://localhost:' + this.address().port);
+});
 
 module.exports = app;
