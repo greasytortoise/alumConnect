d5516affbfb1d43ccd48e25140fd3914c6379d0e
mikejonas <motion@gmail.com>
2016-05-01 23:00:00 -0700

    refactor multer module into middlewear code

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,9 +1,26 @@
 var bodyParser = require('body-parser');
 var util = require('../lib/utility.js');
+var multer  = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'client/assets')
+  },
+  filename: function (req, file, cb) {
+    // console.log(req)
+    console.dir(file);
+    cb(null, file.originalname)
+  }
+});
+var upload = multer({
+  storage: storage,
+  onFileUploadStart: function (file) {
+    console.log(file.name + ' is starting ...');
+  },
+});
+
+
 module.exports = function(app, express) {
-  
   app.use(bodyParser.json());
   app.use(express.static(__dirname + '/../../client'));
-
+  app.use(upload.single('upl'));
 };
-
--- server/config/routes.js
+++ server/config/routes.js
@@ -3,22 +3,6 @@ var db = require('../dbConfig');
 var util = require('../lib/utility.js');
 var bcrypt = require('bcrypt');
 var handler = require('../lib/handler');
-
-var multer  = require('multer');
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'client/assets')
-  },
-  filename: function (req, file, cb) {
-    console.log(req.body)
-    console.log(file);
-    cb(null, file.originalname)
-  }
-})
-var upload = multer({ storage: storage })
-
-
-
 var groupRouter = require('../routers/groupRouter');
 var userRouter = require('../routers/userRouter');
 var siteRouter = require('../routers/siteRouter');
@@ -36,22 +20,7 @@ module.exports = function(app, express) {
   app.post('/checktoken', util.getPermissions);
   app.post('/login', handler.checkLogin);
 
-  app.post('/user/uploadimage',upload.single('upl'), function(req, res) {
-    // console.log(req.body); //form fields
-    // /* example output:
-    // { title: 'abc' }
-    //  */
-    // console.log(req.file); //form files
-    /* example output:
-              { fieldname: 'upl',
-                originalname: 'grumpy.png',
-                encoding: '7bit',
-                mimetype: 'image/png',
-                destination: './uploads/',
-                filename: '436ec561793aa4dc475a88e84776b1b9',
-                path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
-                size: 277056 }
-     */
+  app.post('/user/uploadimage', function(req, res) {
     res.status(204).end();
   })
 
