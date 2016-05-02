570e1e6137462b8b4e6578eb9a72edf537012aff
mikejonas <motion@gmail.com>
2016-05-01 21:37:04 -0700

    Enable uploading images, still needs more work

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,6 +4,21 @@ var util = require('../lib/utility.js');
 var bcrypt = require('bcrypt');
 var handler = require('../lib/handler');
 
+var multer  = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'client/assets')
+  },
+  filename: function (req, file, cb) {
+    console.log(req.body)
+    console.log(file);
+    cb(null, file.originalname)
+  }
+})
+var upload = multer({ storage: storage })
+
+
+
 var groupRouter = require('../routers/groupRouter');
 var userRouter = require('../routers/userRouter');
 var siteRouter = require('../routers/siteRouter');
@@ -21,11 +36,30 @@ module.exports = function(app, express) {
   app.post('/checktoken', util.getPermissions);
   app.post('/login', handler.checkLogin);
 
+  app.post('/user/uploadimage',upload.single('upl'), function(req, res) {
+    // console.log(req.body); //form fields
+    // /* example output:
+    // { title: 'abc' }
+    //  */
+    // console.log(req.file); //form files
+    /* example output:
+              { fieldname: 'upl',
+                originalname: 'grumpy.png',
+                encoding: '7bit',
+                mimetype: 'image/png',
+                destination: './uploads/',
+                filename: '436ec561793aa4dc475a88e84776b1b9',
+                path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
+                size: 277056 }
+     */
+    res.status(204).end();
+  })
+
   app.post('/changepassword', function(req, res) {
     var tokenUserData = req.token.split('.')[1].user;
     Users.where({id: tokenUserData.id}).fetch().then(function(user){
       bcrypt.hash(req.body.password, 10, function(err, hash) {
-        
+
         //DO THING WITH HASH
         //SAVE NEW PW TO DB!
 
