f617d763190556954ec5b7411d3161c6bd367c21
mikejonas <motion@gmail.com>
2016-05-13 09:56:37 -0700

    Rendering group select

--- client/components/frontend/profile/ProfileGroups.js
+++ client/components/frontend/profile/ProfileGroups.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Input, Button } from 'react-bootstrap';
+import Select from 'react-select';
+import RestHandler from '../../../util/RestHandler'
 
 
 class ProfileGroups extends React.Component {
@@ -8,25 +10,47 @@ class ProfileGroups extends React.Component {
     super(props);
     this.state = {
       groups: [],
+      selectedGroups: [],
     };
   }
 
   componentDidMount() {
-    this.setState({groups: this.props.groups})
+    this.setState({selectedGroups: this.props.selectedGroups})
+    this.getAvailableGroups();
   }
 
-  renderProfileSite() {
-    var editing = this.props.editing;
-    var groups = this.state.groups;
+  getAvailableGroups() {
+    RestHandler.Get('/db/groups', (err, res) => {
+      //res.body.map is a Workaround to get the Select valueKey working.
+      //it ads a property with a stringified id to res.body.
+      res.body.map(obj => obj['idString'] = obj['id'].toString())
+      this.setState({groups: res.body});
+    });
+  }
 
-    if(!editing && groups) {
+  handleGroupSelect (selectedGroups) {
+		this.setState({ selectedGroups });
+	}
+
+  renderGroups() {
+    var editing = this.props.editing;
+    var selectedGroups = this.state.selectedGroups;
+
+    if(!editing && selectedGroups) {
       return (
         <div>not editing</div>
       );
     }
     else if(editing) {
       return (
-        <div>editing</div>
+        <Select
+          multi
+          simpleValue
+          disabled={this.state.disabled} value={this.state.selectedGroups} placeholder="Select groups"
+          labelKey="group_name"
+          valueKey="idString"
+          options={this.state.groups}
+          onChange={this.handleGroupSelect.bind(this)} />
       );
     } else {
       return(<div>else</div>)
@@ -36,7 +60,7 @@ class ProfileGroups extends React.Component {
   render() {
     console.log(this.props.groups);
     return (
-      this.renderProfileSite()
+      this.renderGroups()
     )
   }
 }
