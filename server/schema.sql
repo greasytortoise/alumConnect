2f0e8dad4e4b921fa47ec5434d87a48d5d69982b
Alamelu Palaniappan <alam.palaniappan@gmail.com>
2016-05-17 07:34:42 -0700

    fix conflicts

--- server/schema.sql
+++ server/schema.sql
@@ -95,7 +95,9 @@ values
 insert into Users
   (handle, githubid, name, email, image, url_hash, public, permission)
 values
-  ('mbresnan1701', 15022604, 'DONALD TRUMP', 'admin@admin.com', '/assets/photos/trump.jpg', 'ndas2q', 1, 1);
+
+  -- ('your_handle', 'your_id', 'DONALD TRUMP', 'admin@admin.com', '/assets/photos/trump.jpg', 'ndas2q', 0, 1);
+  ('alamuv', 8962181, 'Alamu', 'admin@admin.com', '/assets/photos/trump.jpg', 'ndas2q', 0, 1);
 
 insert into Groups_Users -- new addition
   (user_id, group_id)
