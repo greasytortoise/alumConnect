83bbf0cbeb354abddcf13ab1319b6f4bd7a43d9b
Alamelu Palaniappan <alam.palaniappan@gmail.com>
2016-05-22 22:28:13 -0700

    dashboardusers channge reverted back

--- client/components/admin/DashboardUsers.js
+++ client/components/admin/DashboardUsers.js
@@ -3,7 +3,7 @@ import Griddle from 'griddle-react';
 import RestHandler from '../../util/RestHandler';
 import $ from 'jquery';
 import request from 'superagent';
-import { Modal, Button } from 'react-bootstrap';
+import { Select, Modal, Button } from 'react-bootstrap';
 import { Link } from 'react-router';
 
 var rowMetadata = {
@@ -23,6 +23,7 @@ class DashboardUsers extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
+      visOpts: ['Yes', 'No'],
       users: [],
       key: 666,
       showDelete: false,
@@ -38,19 +39,30 @@ class DashboardUsers extends React.Component {
       var users = res.body;
       for (var i = 0; i < users.length; i++) {
         users[i].Delete = this.getDeleteLink(users[i].id, users[i].name);
-        users[i].Profile = this.getProfileLink(users[i].id);
+        users[i].Name = this.getProfileLink(users[i].id, users[i].name);
+        users[i].Github = this.getGithubLink(users[i].handle);
       }
       this.setState({ users: users });
 
     });
   }
-  getProfileLink(id) {
+  getProfileLink(id, name) {
     return (
       <div className="userLink"
       >
         <Link to={{ pathname: `/users/${id}` }}>
-          View Profile
+          {name}
         </Link>
+      </div>
+   );
+  }
+  getGithubLink(github) {
+    return (
+      <div className="ghLink"
+      >
+        <a href={`https://www.github.com/${github}`}>
+          {github}
+        </a>
       </div>
    );
   }
@@ -99,11 +111,12 @@ class DashboardUsers extends React.Component {
               break;
             }
           }
-          // that.setState({ key: Math.random() });
+          that.setState({ key: Math.random() });
           that.resetDeleteState();
         }
       });
   }
+
 
   closePopup() {
     this.setState({ showDelete: false });
@@ -140,7 +153,7 @@ class DashboardUsers extends React.Component {
           tableClassName='table'
           useGriddleStyles={false}
           resultsPerPage={25}
-          columns={["id", "name", "email", "Profile", "Delete"]}/>
+          columns={["id", "Name", "email", "Github", "Delete"]}/>
       </div>
     );
   }
