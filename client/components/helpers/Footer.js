745d2bf3a959d32241035dfbe3adb7a5ec4c672d
mikejonas <motion@gmail.com>
2016-05-27 17:45:58 -0700

    Implement Footer

---
+++ client/components/helpers/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+var _shuffle = require('lodash/shuffle');
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div id="footer">
+        Built in HR40 by <a href="https://www.linkedin.com/in/mwbresnan" target="_blank">Matt</a>, <a href="https://www.linkedin.com/in/alamu" target="_blank">Alamu</a>, <a href="https://www.linkedin.com/in/yochess" target="_blank">Drake</a>, and <a href="https://www.linkedin.com/in/mikejonas" target="_blank">Jonas</a>
+      </div>
+    );
+  }
+}
+
+module.exports = Footer;
