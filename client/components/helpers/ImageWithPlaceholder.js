1d53a0efddcd5df180887dd945b5ecdfade5dcac
mikejonas <motion@gmail.com>
2016-06-02 13:03:33 -0700

    loading placeholder for images

---
+++ client/components/helpers/ImageWithPlaceholder.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class ImageWithPlaceholder extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      src: './assets/image-loading.gif'
+    };
+  }
+
+  handleImageLoaded() {
+    this.setState({ src: 'https://www.hackreactorconnect.com/' + this.props.src });
+  }
+
+  handleImageErrored() {
+    //TODO: set state to image failed to load image
+  }
+
+  render() {
+    return (
+      <div>
+        <img
+          src={this.state.src}
+          onLoad={this.handleImageLoaded.bind(this)}
+          onError={this.handleImageErrored.bind(this)}
+          />
+      </div>
+    );
+  }
+}
+export default ImageWithPlaceholder;
